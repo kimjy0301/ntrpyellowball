283e940080ea7878754c8b4f01173715d99d06f1
Kim JiYong <znzld505@naver.com>
2021-01-21 15:33:07 +0900

    mmghjkfgh

--- components/KakaoMap.js
+++ components/KakaoMap.js
@@ -44,6 +44,31 @@ const KakaoMap = ()=>{
                 // 마커가 지도 위에 표시되도록 설정합니다
                 marker.setMap(map);  
 
+                function setMapType(maptype) { 
+                    var roadmapControl = document.getElementById('btnRoadmap');
+                    var skyviewControl = document.getElementById('btnSkyview'); 
+                    if (maptype === 'roadmap') {
+                        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);    
+                        roadmapControl.className = 'selected_btn';
+                        skyviewControl.className = 'btn';
+                    } else {
+                        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);    
+                        skyviewControl.className = 'selected_btn';
+                        roadmapControl.className = 'btn';
+                    }
+                }
+                
+                // 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+                function zoomIn() {
+                    map.setLevel(map.getLevel() - 1);
+                }
+                
+                // 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+                function zoomOut() {
+                    map.setLevel(map.getLevel() + 1);
+                }
+                
+
            
             });
           };
