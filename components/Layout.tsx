a348e7568eb9c57a78bcaded5d9bdca28df8bb74
Kim JiYong <znzld505@naver.com>
2021-01-21 15:41:25 +0900

    move footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,16 +4,18 @@ const Layout = (props) => {
   return (
     <div>
       <Header></Header>
-      <div className="content">{props.children}</div>
-      <footer className="fixed bottom-0 w-full justify-center items-end flex bg-gradient-to-br from-yellow-1 via-green-1 to-yellow-1 text-white p-5 border-t-2 border-white text-sm">
-        <span className="text-stroke">Powered By </span>
-        <a href="https://band.us/band/55139316">
-          <img
-            src={`BI-vertical.png`}
-            className="h-10 ml-3 transform hover:scale-110 focus:scale-110 transition-all"
-          ></img>
-        </a>
-      </footer>
+      <div className="content">
+        {props.children}
+        <footer className="absolute bottom-0 w-full justify-center items-end flex bg-gradient-to-br from-yellow-1 via-green-1 to-yellow-1 text-white p-5 border-t-2 border-white text-sm">
+          <span className="text-stroke">Powered By </span>
+          <a href="https://band.us/band/55139316">
+            <img
+              src={`BI-vertical.png`}
+              className="h-10 ml-3 transform hover:scale-110 focus:scale-110 transition-all"
+            ></img>
+          </a>
+        </footer>
+      </div>
     </div>
   );
 };
