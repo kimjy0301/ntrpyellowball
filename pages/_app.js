283c3583e1061bce1d305c2d59a0b34dd351927e
Kim JiYong <znzld505@naver.com>
2021-01-22 12:53:58 +0900

    add questions

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,7 @@ import Layout from "../components/Layout";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
-
-  document.addEventListener("touchstart", function() {}, true);
-  
+ 
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/forehand/index.tsx
+++ pages/forehand/index.tsx
@@ -1,42 +1,19 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import { useEffect, useRef } from "react";
-import { LegacyRef } from "react";
+import Question from "../../components/Question";
+import { questions } from "../../components/data";
 
 const index = () => {
-  const carouselRef: LegacyRef<Carousel> = useRef();
-
-  useEffect(() => {
-    // carouselRef.current?.onClickNext();
-  }, []);
-
-  const onbtnClick = () => {
-    carouselRef.current?.onClickNext();
-  };
-
   return (
-    <div className="h-16">
-      <div>
-        <h1>Q : 볼의 컨트롤이 좋고 스핀으로 위력적인 샷을 구사 할 수 있다.</h1>
-        <div>
-          <button onClick={onbtnClick}>O</button>
-          <button onClick={onbtnClick}>X</button>
-        </div>
-      </div>
-      <div>
-        <h1>Q : 강한 볼을 치기위해 포핸드를 많이 사용한다.</h1>
-        <div>
-          <button onClick={onbtnClick}>O</button>
-          <button onClick={onbtnClick}>X</button>
-        </div>
-      </div>
-      <div>
-        <h1>Q : 상황에 따라 좋은 터치를 구사하며 패싱샷을 적절히 구사한다.</h1>
-        <div>
-          <button onClick={onbtnClick}>O</button>
-          <button onClick={onbtnClick}>X</button>
-        </div>
-      </div>
+    <div className="flex flex-col items-center py-2 mx-auto h-full w-full overflow-y-auto relative">
+      {questions.map((value, i) => {
+        return (
+          <Question
+            key={i}
+            question={value}
+            onCLickYes={() => {}}
+            onClickNo={() => {}}
+          ></Question>
+        );
+      })}
     </div>
   );
 };
