144de2900dca0a973620075e893eb98b6a3ee0e1
Kim JiYong <znzld505@naver.com>
2021-01-22 14:29:05 +0900

    change transition duration

--- components/Question.tsx
+++ components/Question.tsx
@@ -5,7 +5,7 @@ const Question = ({ question, onClickYes, onClickNo, show }) => {
 
   useEffect(() => {
     setLoaded(true);
-  }, [loaded]);
+  }, []);
 
   if (show === false) {
     return <></>;
@@ -15,7 +15,7 @@ const Question = ({ question, onClickYes, onClickNo, show }) => {
         <div
           className={`${
             loaded ? "opacity-100" : "opacity-0"
-          } duration-500 w-96 flex relative flex-col h-48 md:shadow-lg justify-center border-yellow-1 border-2  rounded-md items-center p-4 bg-white mx-1 my-1 shadow-lg md:max-w-xs1 transition-all`}
+          } duration-700 w-96 flex relative flex-col h-48 md:shadow-lg justify-center border-yellow-1 border-2  rounded-md items-center p-4 bg-white mx-1 my-1 shadow-lg md:max-w-xs1 transition-all`}
         >
           <span className="text-xl">{question}</span>
           <div className="w-full border-t-2 my-3 border-gray-100 "></div>
