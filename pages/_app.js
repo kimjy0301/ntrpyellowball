4db7cf3af049be12485d9e691c989ee20281d054
Kim JiYong <znzld505@naver.com>
2021-01-22 10:29:32 +0900

    toch

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import Layout from "../components/Layout";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
+
+  document.addEventListener("touchstart", function() {}, true);
+  
   return (
     <Layout>
       <Component {...pageProps} />
