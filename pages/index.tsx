bd92eddf2dc6563b6282af01506cd7ac1b764430
Kim JiYong <znzld505@naver.com>
2021-01-28 11:26:20 +0900

    change main menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,14 +29,20 @@ const Home = (props: HomeProps) => {
             <div className="w-full h-full bg-gradient-to-t from-yellow-1 to-yellow-2 transform absolute inset-0 rounded-xl -rotate-6 shadow-lg md:h-64"></div>
             <div className="z-10 bg-white p-5 rounded-lg shadow-lg md:h-64">
               <h1 className="text-2xl font-semibold mt-5">노란공테니스 YTRP</h1>
-              <h1 className="text-sm tracking-normal">
-                (Yellowball Tennis Rating Program)
+              <h1 className="text-sm tracking-normal ml-8">
+                www.yellowball.co.kr
               </h1>
-              <h1 className="mt-3">해당 프로그램은</h1>
-              <h1 className="">순수 동호인을 대상으로 하여</h1>
-              <h1 className="">자가 테니스레벨을 테스트하는 프로그램이며</h1>
+              <h1 className="mt-3">누가 뭐래도</h1>
+              <h1 className="">
+                테니스는 <span className="text-blue-600">건강</span>이고{" "}
+                <span className="text-red-600">즐거움</span>이 우선 입니다
+              </h1>
+              <h1 className="">적당한 열정과 적당한 욕심으로</h1>
 
-              <h1 className="">테니스의 상식적인 이야기 입니다.</h1>
+              <h1 className="">
+                <span className="text-red-600">건강한 땀</span>을 흘리고 언제나{" "}
+                <span className="text-blue-600">즐테</span> 하기를 ..
+              </h1>
             </div>
           </div>
           <div className="flex justify-center items-center flex-row my-5 flex-wrap max-w-2xl mt-12 md:mt-0">
@@ -47,7 +53,11 @@ const Home = (props: HomeProps) => {
               }}
               className="flex md:m-3 md:w-72 md:shadow-lg justify-between border-yellow-1 border-2  rounded-md items-center p-4  w-2/5 bg-white mx-1 my-1 shadow-lg md:max-w-xs cursor-pointer hover:bg-yellow-1 transition-all hover:shadow-lg"
             >
-              <span className="text-sm md:text-2xl">테크닉 능력</span>
+              <span className="text-sm md:text-2xl">
+                <span className="text-blue-600">T</span>echnic{" "}
+                <span className="text-blue-600">L</span>evel{" "}
+                <span className="text-blue-600">T</span>est
+              </span>
             </div>
 
             <div
@@ -57,7 +67,11 @@ const Home = (props: HomeProps) => {
                 router.push("/game");
               }}
             >
-              <span className="text-sm md:text-2xl">게임 운영능력</span>
+              <span className="text-sm md:text-2xl">
+                <span className="text-red-600">G</span>ame{" "}
+                <span className="text-red-600">L</span>evel{" "}
+                <span className="text-red-600">T</span>est
+              </span>
             </div>
 
             <div
@@ -67,7 +81,7 @@ const Home = (props: HomeProps) => {
                 router.push("/develop");
               }}
             >
-              <span className="text-sm md:text-2xl">규칙과 매너</span>
+              <span className="text-sm md:text-2xl">테니스 코트 정보</span>
             </div>
 
             <div
@@ -77,7 +91,7 @@ const Home = (props: HomeProps) => {
                 router.push("/develop");
               }}
             >
-              <span className="text-sm md:text-2xl">열정 테스트</span>
+              <span className="text-sm md:text-2xl">테니스 레슨정보</span>
             </div>
 
             <div
@@ -87,7 +101,7 @@ const Home = (props: HomeProps) => {
                 router.push("/develop");
               }}
             >
-              <span className="text-sm md:text-2xl">테니스 상식</span>
+              <span className="text-sm md:text-2xl">테니스 클럽 정보</span>
             </div>
 
             <div
@@ -97,7 +111,16 @@ const Home = (props: HomeProps) => {
                 router.push("/develop");
               }}
             >
-              <span className="text-sm md:text-2xl">테니스 용어</span>
+              <span className="text-sm md:text-2xl">실내 연습장 정보</span>
+            </div>
+            <div
+              className="flex md:m-3 md:w-72 md:shadow-lg justify-between border-yellow-1 border-2  rounded-md items-center p-4  w-2/5 bg-white mx-1 my-1 shadow-lg md:max-w-xs cursor-pointer hover:bg-yellow-1 transition-all hover:shadow-lg"
+              onClick={(e) => {
+                e.preventDefault();
+                router.push("/develop");
+              }}
+            >
+              <span className="text-sm md:text-2xl">테니스 규칙과 매너</span>
             </div>
             <div
               className="flex md:m-3 md:w-72 md:shadow-lg justify-between border-yellow-1 border-2  rounded-md items-center p-4  w-2/5 bg-white mx-1 my-1 shadow-lg md:max-w-xs cursor-pointer hover:bg-yellow-1 transition-all hover:shadow-lg"
@@ -107,15 +130,6 @@ const Home = (props: HomeProps) => {
               }}
             >
               <span className="text-sm md:text-2xl">테니스 한마디</span>
-            </div>
-            <div
-              className="flex md:m-3 md:w-72 md:shadow-lg justify-between border-yellow-1 border-2  rounded-md items-center p-4  w-2/5 bg-white mx-1 my-1 shadow-lg md:max-w-xs cursor-pointer hover:bg-yellow-1 transition-all hover:shadow-lg"
-              onClick={(e) => {
-                e.preventDefault();
-                router.push("/develop");
-              }}
-            >
-              <span className="text-sm md:text-2xl">좋은 글 한마디</span>
             </div>
           </div>
 
