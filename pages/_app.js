adee7489d466094d87e0dc30fc186ef6c1042b9d
Kim JiYong <znzld505@naver.com>
2021-02-01 11:33:45 +0900

    아티클 테스트

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import Layout from "../components/Layout";
 import "../styles/index.css";
+// Import Swiper styles
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
---
+++ pages/article/index.tsx
@@ -0,0 +1,77 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/swiper-bundle.css";
+
+const index = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 200);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>노란공테니스 - 테니스 한마디</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="w-full h-full flex justify-center items-center flex-col p-3">
+        <div
+          className={`flex h-full w-full relative flex-col md:shadow-lg justify-center border-yellow-1 border-2  rounded-md items-center p-4 bg-white mx-1 my-1 shadow-lg md:max-w-xs1`}
+        >
+          <div
+            className={`${
+              loaded ? "opacity-100" : "opacity-0"
+            } duration-700 transition-all h-full w-full`}
+          >
+            <div className="w-full h-full flex items-center justify-center">
+              <Swiper
+                className="w-1/3"
+                spaceBetween={50}
+                slidesPerView={1}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log("slide change")}
+              >
+                <SwiperSlide>
+                  <img
+                    src="article/article_1.png"
+                    className="w-full h-96"
+                  ></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="article/article_1.png"
+                    className="w-full h-96"
+                  ></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="article/article_1.png"
+                    className="w-full h-96"
+                  ></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="article/article_1.png"
+                    className="w-full h-96"
+                  ></img>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <img
+                    src="article/article_1.png"
+                    className="w-full h-96"
+                  ></img>
+                </SwiperSlide>
+              </Swiper>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default index;
